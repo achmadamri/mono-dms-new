558a04996689778a75517ec0d926398030fec9e5
Achmad Amri <achmad.amri@gmail.com>
2023-10-08 19:31:10 +0700

    Update

--- bsd-front-dms-material/src/app/util.ts
+++ bsd-front-dms-material/src/app/util.ts
@@ -67,7 +67,7 @@ export class Util {
 
         }, {
                 type: type,
-                timer: 60000,
+                timer: 1000 * 60 * 60,
                 placement: {
                     from: from,
                     align: align
